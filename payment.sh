ba47889e72ce6138f80f4f1f3e0a356bc5deb964
Chimtamreddy <kr7348202@gmail.com>
2024-01-25 18:34:29 +0530

    Password NFRS

--- payment.sh
+++ payment.sh
@@ -1,6 +1,6 @@
 component=payment
 source common.sh
-if [ -z "${rabbitmq_root_password}" ]; then
+if [ -z "${rabbitmq_app_password}" ]; then
   echo Input RabbitMQ AppUser Password Missing
   exit 1
 fi
