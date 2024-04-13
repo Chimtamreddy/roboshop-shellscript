df14e0de346769377c655c804e4b7a5d9bc09a6a
Chimtamreddy <kr7348202@gmail.com>
2024-04-13 21:41:38 +0530

    shellscript

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 func_nodejs() {
   log=/tmp/roboshop.log
-  echo -e "\e[36m>>>>>>>>>>>>>Create user Service <<<<<<<<<<<<<<<<<\e[0m"
-  cp user.service /etc/systemd/system/user.service &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>>Create ${component} Service <<<<<<<<<<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Create mongo Reo <<<<<<<<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Install Nodejs Disable and Enable Service <<<<<<<<<<<<<<<<<\e[0m"
@@ -16,19 +16,19 @@ func_nodejs() {
   echo -e "\e[36m>>>>>>>>>>>>>Create Application Service <<<<<<<<<<<<<<<<<\e[0m"
   mkdir /app &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Download Application Content <<<<<<<<<<<<<<<<<\e[0m"
-  curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log}
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
   cd /app
   echo -e "\e[36m>>>>>>>>>>>>>Extract Content <<<<<<<<<<<<<<<<<\e[0m"
-  unzip /tmp/user.zip &>>${log}
+  unzip /tmp/${component}.zip &>>${log}
   cd /app
   echo -e "\e[36m>>>>>>>>>>>>>Install NPM Service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   npm install &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Create Mongodb Service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   dnf install mongodb-org-shell -y &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Schema <<<<<<<<<<<<<<<<<\e[0m"
-  mongo --host mongodb.kr7348202.online </app/schema/user.js &>>${log}
-  echo -e "\e[36m>>>>>>>>>>>>>Start user Service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+  mongo --host mongodb.kr7348202.online </app/schema/${component}.js &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>>Start ${component} Service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   systemctl daemon-reload &>>${log}
-  systemctl enable user &>>${log}
-  systemctl restart user &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
 }
