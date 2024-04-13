2f4e0d4c2476c090c3cb73187e053951fe757677
Chimtamreddy <kr7348202@gmail.com>
2024-04-13 21:54:47 +0530

    shellscript

--- common.sh
+++ common.sh
@@ -1,14 +1,6 @@
-func_nodejs() {
-  log=/tmp/roboshop.log
-  echo -e "\e[36m>>>>>>>>>>>>>Create ${component} Service <<<<<<<<<<<<<<<<<\e[0m"
-  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-  echo -e "\e[36m>>>>>>>>>>>>>Create mongo Reo <<<<<<<<<<<<<<<<<\e[0m"
-  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  echo -e "\e[36m>>>>>>>>>>>>>Install Nodejs Disable and Enable Service <<<<<<<<<<<<<<<<<\e[0m"
-  dnf module disable nodejs -y &>>${log}
-  dnf module enable nodejs:18 -y &>>${log}
-  echo -e "\e[36m>>>>>>>>>>>>>Create Nodejs Service <<<<<<<<<<<<<<<<<\e[0m"
-  dnf install nodejs -y &>>${log}
+log=/tmp/roboshop.log
+
+func_apppreq() {
   echo -e "\e[36m>>>>>>>>>>>>>Create Application Service <<<<<<<<<<<<<<<<<\e[0m"
   useradd roboshop &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Remove Exiting Content <<<<<<<<<<<<<<<<<\e[0m"
@@ -21,6 +13,27 @@ func_nodejs() {
   echo -e "\e[36m>>>>>>>>>>>>>Extract Content <<<<<<<<<<<<<<<<<\e[0m"
   unzip /tmp/${component}.zip &>>${log}
   cd /app
+}
+
+func_systmed() {
+  echo -e "\e[36m>>>>>>>>>>>>>Start ${component} Service <<<<<<<<<<<<<<<<<\e[0m"
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
+}
+
+func_nodejs() {
+  
+  echo -e "\e[36m>>>>>>>>>>>>>Create ${component} Service <<<<<<<<<<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>>Create mongo Reo <<<<<<<<<<<<<<<<<\e[0m"
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>>Install Nodejs Disable and Enable Service <<<<<<<<<<<<<<<<<\e[0m"
+  dnf module disable nodejs -y &>>${log}
+  dnf module enable nodejs:18 -y &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>>Create Nodejs Service <<<<<<<<<<<<<<<<<\e[0m"
+  dnf install nodejs -y &>>${log}
+  func_apppreq
   echo -e "\e[36m>>>>>>>>>>>>>Install NPM Service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   npm install &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Create Mongodb Service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
@@ -28,7 +41,20 @@ func_nodejs() {
   echo -e "\e[36m>>>>>>>>>>>>>Schema <<<<<<<<<<<<<<<<<\e[0m"
   mongo --host mongodb.kr7348202.online </app/schema/${component}.js &>>${log}
   echo -e "\e[36m>>>>>>>>>>>>>Start ${component} Service <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl restart ${component} &>>${log}
+  func_systmed
+}
+
+java_mavne() {
+  echo -e "\e[36m>>>>>>>>>>>>>Start ${component} Service <<<<<<<<<<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>>Start ${component} Service <<<<<<<<<<<<<<<<<\e[0m"
+  dnf install maven -y &>>${log}
+  func_apppreq
+  mvn clean package &>>${log}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  
+  dnf install mysql -y &>>${log}
+  mysql -h mysql.kr7348202.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+  func_systmed
+
 }
